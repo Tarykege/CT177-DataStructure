e3b999ad6f5172a88b60cfa06d73c2c21dcb50b5
Tarykege <xoiiwkingg@gmail.com>
2021-09-17 15:19:57 +0700

    complete_Stack

--- Stack.cpp
+++ Stack.cpp
@@ -1,3 +1,68 @@
+#include<stdio.h>
 
+#define MaxLength 300
+typedef int ElementType;
+typedef struct{
+    ElementType element[MaxLength];
+    int top_index;
+}Stack;
 
+void makeNullStack(Stack *S){
+    S->top_index=MaxLength;
+}
+
+int emptyStack(Stack S){
+    return S.top_index==MaxLength;
+}
+
+int fullStack(Stack S){
+    return S.top_index==0;
+}
+
+ElementType top(Stack S){
+    if(emptyStack(S)){
+        printf("Get top Error");
+    }
+    else return S.element[S.top_index];
+
+}
+
+void pop(Stack *S){
+    if(emptyStack(*S)){
+        printf("Pop error");
+    }
+    else S->top_index++;
+}
+
+void push(ElementType x,Stack *S){
+    if(fullStack(*S)){
+        printf("Push error");
+    }
+    else {
+        S->top_index--;
+        S->element[S->top_index]=x;
+    }
+}
+
+int main(){
+    Stack S;
+    makeNullStack(&S);
+    printf("emptyStack = %d\n",emptyStack(S));
+    push(1,&S);
+    printf("%d\n",top(S));
+    push(2,&S);
+    printf("%d\n",top(S));
+    push(3,&S);
+    printf("%d\n",top(S));
+    push(4,&S);
+    printf("%d\n",top(S));
+    pop(&S);
+    printf("%d\n",top(S));
+    pop(&S);
+    printf("%d\n",top(S));
+    pop(&S);
+    printf("%d\n",top(S));    
+    return 0;
+
+}
 
